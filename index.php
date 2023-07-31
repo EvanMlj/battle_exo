36fd13628735ecfe3390135ec48676b725bfec74
EvanMlj <120018761+EvanMlj@users.noreply.github.com>
2023-07-31 11:00:23 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exo battle</title>
+</head>
+
+<body>
+    <h1>Battle</h1>
+    <div class="prematch">
+        <form action="index.html" method="post">
+            <div>
+                <h2>Joueur</h2>
+                <img src="asset/quick-man.svg" alt="icone du joueur" height="150" width="150">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+            </div>
+
+            <div>
+                <h2>Adversaire</h2>
+                <img src="asset/cyborg-face.svg" alt="icone de l'adversaire" height="150" width="150">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+                <input type="text" name="" id="">
+            </div>
+            <input type="submit" value="FIGHT">
+        </form>
+
+    </div>
+</body>
+
+</html>
